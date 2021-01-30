fc2848b5f36a1e550ec6beb7fa5a838d1a4b3a20
Hadryel <hadryelruda@gmail.com>
2021-01-30 17:25:41 -0200

    Added rocks, first version of wave animation and obstacle spawner

---
+++ Assets/Scripts/ShipObstacleSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipObstacleSpawner : MonoBehaviour
+{
+    public GameObject[] RockPrefabs;
+
+    void Start()
+    {
+        SpawnRock(RockPrefabs[0], new Vector2(12, 5));
+        SpawnRock(RockPrefabs[1], new Vector2(12, -5));
+        SpawnRock(RockPrefabs[2], new Vector2(-12, 5));
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRock(GameObject rockPrefab, Vector2 position)
+    {
+        var rockGO = Instantiate(rockPrefab, position, Quaternion.identity);
+
+        //var ship = shipGO.GetComponent<Ship>();
+        //ship.Direction = Ship.GetDirectionFrom(direction);
+        //ship.Spawner = this;
+    }
+}
