17e6464383327de655c3c2f243ef7e4968ca1776
Hadryel <hadryelruda@gmail.com>
2021-01-31 12:25:32 -0200

    Fix to maximum volum, started replay screen

---
+++ Assets/Scripts/SFX/VolumeSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSetup : MonoBehaviour
+{
+    void OnEnable()
+    {
+        // TODO: Use PlayerPref to set this (and let it be configured in settings)
+        AudioListener.volume = 0.05f;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
